a45fd416c0dd21a5255b4ff43847ea8dafbf3d74
Sang eun Lee <sanlee@e1z3r7p25.42.us.org>
2020-03-02 12:18:52 -0800

    .

--- cadet/get_next_line/get_next_line.c
+++ cadet/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sanlee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 20:46:15 by sanlee            #+#    #+#             */
-/*   Updated: 2020/03/01 16:58:08 by sanlee           ###   ########.fr       */
+/*   Updated: 2020/03/02 12:16:41 by sanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int					get_next_line(const int fd, char **line)
 	ssize_t			a;
 	int				end;
 
-    // error -1
 	if (fd < 0 || !line || (!c[fd] && !(c[fd] = ft_strnew(1))))
 		return (-1);
 	while (!(ft_strchr(c[fd], '\n')) && (a = read(fd, buffer, BUFF_SIZE)) > 0)
@@ -31,7 +30,6 @@ int					get_next_line(const int fd, char **line)
 		c[fd] = ft_strjoin(c[fd], buffer);
 		ft_strdel(&tmp);
 	}
-    // read -1 0
 	if (a == -1 || !*(tmp = c[fd]))
 		return ((a == -1) ? -1 : 0);
 	if ((end = (ft_strchr(c[fd], '\n') > 0)))
@@ -41,6 +39,5 @@ int					get_next_line(const int fd, char **line)
 	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + end),
 			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + end)));
 	ft_strdel(&tmp);
-    // 1, 0
 	return (!(!c[fd] && !ft_strlen(*line)));
 }
