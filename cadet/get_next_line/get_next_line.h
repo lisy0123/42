c817b4d9e193d2937ab9f0c28979691fcfa222e5
Sang eun Lee <sanlee@e1z3r10p5.42.us.org>
2020-03-03 22:41:08 -0800

    .

--- cadet/get_next_line/get_next_line.h
+++ cadet/get_next_line/get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: sanlee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 21:48:07 by sanlee            #+#    #+#             */
-/*   Updated: 2020/03/02 14:36:19 by sanlee           ###   ########.fr       */
+/*   Updated: 2020/03/03 22:40:17 by sanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
+# include "libft/libft.h"
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/types.h>
