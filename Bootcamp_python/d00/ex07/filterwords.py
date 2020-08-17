b1c16b9134b76b3b5a906e621b97e0a96b3f98bd
lisy0123 <leesese3320@gmail.com>
2020-08-17 20:18:11 +0900

    ex07 finish

--- Bootcamp_python/d00/ex07/filterwords.py
+++ Bootcamp_python/d00/ex07/filterwords.py
@@ -1,3 +1,25 @@
+"""
+- Rmoves all the words in a string. 
+- Shorter than or equal to n letters. 
+- Returns the filtered list with no punctuation. 
+- Two parameters: a string, and an integer n
+"""
+
 import sys
 
-
+try:
+	if len(sys.argv) == 3:
+		NUM1 = (int)(sys.argv[2])
+		STR1 = (str)(sys.argv[1]).split()
+		if STR1[0].isalpha() == True:
+			LIST=[]
+			for i in range(len(STR1)):
+				if len(STR1[i]) > NUM1:
+					LIST.append(STR1[i])
+			print("{}".format(LIST))
+		else:
+			print("ERROR1")
+	else:
+		print("ERROR2")
+except:
+	print("ERROR3")
